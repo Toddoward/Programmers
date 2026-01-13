91a90b624177ad67d3e093dddbe01d901c621fc0
Toddoward <91007890+Toddoward@users.noreply.github.com>
2026-01-13 12:38:43 +0900

    [level 2] Title: 숫자의 표현, Time: 1.75 ms, Memory: 9.15 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -0,0 +1,9 @@
+def solution(n):
+    answer = 0     
+    for i in range(1, int(n**0.5) + 1):
+        if n % i == 0:
+            if i % 2 != 0:
+                answer += 1
+            if (n // i) % 2 != 0 and (i * i) != n:
+                answer += 1
+    return answer
