8666ce2d038f1ec9f4f3c1f6ade9336b598b1485
Toddoward <91007890+Toddoward@users.noreply.github.com>
2026-01-21 11:30:23 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 0.01 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.py
+++ 프로그래머스/0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.py
@@ -1,3 +1,2 @@
 def solution(my_str, n):
-    amount = len(my_str) // n if len(my_str) % n == 0 else (len(my_str) // n) + 1
-    return [my_str[i*n:i*n + n] for i in range(amount)]
+    return [my_str[i:i + n] for i in range(0, len(my_str), n)]
