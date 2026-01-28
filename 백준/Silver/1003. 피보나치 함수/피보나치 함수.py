7811e9b5ff273e59cc9b5e002a6fcf54f9c6abd4
Toddoward <91007890+Toddoward@users.noreply.github.com>
2026-01-28 14:43:56 +0900

    [Silver III] Title: 피보나치 함수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -1,21 +1,20 @@
 import sys
 input = sys.stdin.readline
 
-N = int(input())
-lines = [int(input().strip()) for _ in range(N)]
+def mul(A, B):
+    return [[sum(a * b for a, b in zip(ra, cb)) for cb in zip(*B)] for ra in A]
 
-for n_val in lines:
-    if n_val == 0:
+for _ in range(int(input())):
+    n = int(input())
+    if n == 0:
         print(1, 0)
         continue
     
     res, base = [[1, 0], [0, 1]], [[1, 1], [1, 0]]
-    temp = n_val
-    while temp > 0:
-        if temp % 2 == 1:
-            
-            res = [[sum(res[r][k] * base[k][j] for k in range(2)) for j in range(2)] for r in range(2)]
-        base = [[sum(base[r][k] * base[k][j] for k in range(2)) for j in range(2)] for r in range(2)]
-        temp //= 2
+    while n > 0:
+        if n % 2:
+            res = mul(res, base)
+        base = mul(base, base)
+        n //= 2
     
     print(res[1][1], res[0][1])
